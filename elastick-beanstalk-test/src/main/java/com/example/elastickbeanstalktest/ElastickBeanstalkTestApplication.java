dc50350e30c706d5a6cf77363fed01927d877458
soichiro <soichiroseike@gmail.com>
2018-11-09 13:07:17 +0900

    first commit

---
+++ elastick-beanstalk-test/src/main/java/com/example/elastickbeanstalktest/ElastickBeanstalkTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.elastickbeanstalktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElastickBeanstalkTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElastickBeanstalkTestApplication.class, args);
+	}
+}
